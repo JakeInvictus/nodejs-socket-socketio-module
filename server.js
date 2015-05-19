4d0591affa31c19ed3dae74ecf15f237d5071326
Jack Invictus <jackinvictus@outlook.com>
2015-05-19 16:45:58 -0700

    Emitting and receiving events

--- server.js
+++ server.js
@@ -2,4 +2,8 @@ var io = require('socket.io'),
   connect = require('connect');
 
 var app = connect().use(connect.static('public')).listen(3000);
-var socket = io.listen(app);
+var chat_room = io.listen(app); //chat_room is a server
+
+chat_room.sockets.on('connection', function(socket) {
+  chat_room.sockets.emit('entrance', {message: 'A new chatter is online.'});
+});
