5c7d45fafbcb5f16969f9bbf845821827b05206d
Jack Invictus <jackinvictus@outlook.com>
2015-05-19 19:54:27 -0700

    Messaging across sockets

--- server.js
+++ server.js
@@ -4,11 +4,15 @@ var io = require('socket.io'),
 var app = connect().use(connect.static('public')).listen(3000);
 var chat_room = io.listen(app); //chat_room is a server
 
-chat_room.sockets.on('connection', function(socket) {
+chat_room.sockets.on('connection', function (socket) {
   socket.emit('entrance', {message: 'Welcome to the chat room!'});
 
   socket.on('disconnect', function() {
   	chat_room.sockets.emit('exit', {message: 'A chatter has disconnected.'});
+  });
+
+  socket.on('chat', function (data) {
+  	chat_room.sockets.emit('chat', {message: '# ' + data.message});
   });
 
   chat_room.sockets.emit('entrance', {message: 'A new chatter is online.'});
